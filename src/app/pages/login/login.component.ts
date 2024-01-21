df699365e724591cc65bbb25ffbe4e66588e08f9
ali-bouali <bouali.pro@gmail.com>
2024-01-21 20:46:17 +0100

    create login / register comps

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  login() {
+
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+
+  }
 }
