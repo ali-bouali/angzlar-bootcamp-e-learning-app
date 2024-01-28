12ae57739d94ca319f2699b5509a04c749091783
ali-bouali <bouali.pro@gmail.com>
2024-01-28 20:47:32 +0100

    implement login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/auth/authentication.service';
+import {AuthRequest} from '../../models/auth-request';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,31 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent {
 
+  authRequest: AuthRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthenticationService
   ) {
   }
 
   login() {
-
+    this.errorMsg = [];
+    this.authService.login(this.authRequest)
+      .subscribe({
+        next: (res) => {
+          this.router.navigate(['student']);
+        },
+        error: (err) => {
+          console.log(err);
+          if (err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          } else {
+            this.errorMsg.push(err.error.errorMsg);
+          }
+        }
+      });
   }
 
   register() {
--- src/app/services/auth/authentication.service.ts
+++ src/app/services/auth/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RegisterRequest} from '../../models/register-request';
+import {AuthRequest} from '../../models/auth-request';
+import {AuthResponse} from '../../models/auth-response';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,7 @@ import {RegisterRequest} from '../../models/register-request';
 export class AuthenticationService {
 
   baseUrl = 'http://localhost:8080';
+  // eyJhbGciOiJIUzUxMiJ9.eyJmaXJzdG5hbWUiOiJBbGkiLCJzdWIiOiJhbGlAbWFpbC5jb20iLCJpYXQiOjE3MDY0NzEwNzksImV4cCI6MTcwNjU1NzQ3OX0.AuSdBk5O9je-75Ywyk8wLV2DllZCuYY-1gplU39NHUOJaLKuomsZT4Y7YoqoIdO1vOn_uQKseVE54EkLn-ZuAQ
 
   constructor(
     private http: HttpClient
@@ -15,5 +18,9 @@ export class AuthenticationService {
 
   register(registerRequest: RegisterRequest) {
     return this.http.post<any>(`${this.baseUrl}/auth/register`, registerRequest);
+  }
+
+  login(registerRequest: AuthRequest) {
+    return this.http.post<AuthResponse>(`${this.baseUrl}/auth/login`, registerRequest);
   }
 }
