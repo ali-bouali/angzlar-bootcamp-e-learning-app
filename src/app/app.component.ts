c645fbff87a5f45e912030c630a3aee0fd61e815
ali-bouali <bouali.pro@gmail.com>
2024-01-28 20:07:46 +0100

    cleanup code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import {MyFirstService} from './services/s1/my-first.service';
-import {MyFirstRestService} from './services/rest/my-first-rest.service';
-import {lastValueFrom} from 'rxjs';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,38 +6,4 @@ import {lastValueFrom} from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'e-learning';
-  result = 0;
-
-  constructor(
-    private myService: MyFirstService,
-    private restService: MyFirstRestService
-  ) {}
-
-
-  calculateSum() {
-    this.restService.findAllStudents()
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-          console.log('REST Call result received');
-        },
-        error: (err) => {
-          console.log('REST call error');
-          console.log(err);
-        },
-        complete: () => {
-          console.log('REST call done');
-        }
-      });
-
-    this.result = this.myService.sum(3, 5);
-    // some code
-  }
-
-  async asyncCall() {
-    // promise
-    const res = await lastValueFrom(this.restService.findAllStudents());
-    this.result = this.myService.sum(Math.random(), 5);
-  }
 }
